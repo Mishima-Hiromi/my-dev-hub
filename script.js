e01408c0afb46877268842e7864fe57ad7f2b725
Mishima-Hiromi <hiromishi21@gmail.com>
2025-12-27 21:15:28 +0900

    feat: 色を相互に変換

--- script.js
+++ script.js
@@ -2,9 +2,24 @@
 const btn = document.getElementById("color-btn");
 const title = document.getElementById("title");
 
-// 2. イベントを監視する（Event Listener）
+// --- 2. データの管理 (State) ---
+// アプリが今どんな状態かを、変数で持っておくのが「不変の基礎」です
+let isRed = false;
+
+// --- 3. 振る舞いの定義 (Event Handling) ---
 btn.addEventListener("click", () => {
+  // 状態を反転
+  isRed = !isRed;
+
   // 3. 状態を変化させる
-  title.style.color = "red";
-  console.log("ボタンが押されました！");
+  if (isRed) {
+    title.innerText = "赤になりました！";
+    title.style.color = "red";
+  } else {
+    title.innerText = "Hello,World";
+    title.style.color = "black";
+  }
+
+  //デバッグ
+  console.log("現在の状態:", isRed);
 });
